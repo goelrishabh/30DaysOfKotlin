c5f1ee6ff649b3299542061cc7a2db6c3956dab1
Droid Developer <appdev@aerogram.in>
2020-05-23 23:16:48 +0530

    Enum fun

--- Sample/kotlin/src/main/java/com/goelrishabh/kotlin/Enums.kt
+++ Sample/kotlin/src/main/java/com/goelrishabh/kotlin/Enums.kt
@@ -11,36 +11,65 @@ fun main() {
     println(AccountType.PLATINUM.discountPercentage)
     println(AccountType.PLATINUM.subs)
 
-    println(AccountType.BRONZE.calculate())
+    println(AccountType.BRONZE.calculateAbstract())
+
+    for (value in AccountType.values()) {
+        println(value)
+    }
+    println()
+    // OR
+    println()
+    AccountType.values().forEach {
+        println(it)
+    }
+
+    println()
+
+    println(AccountType.calculate(AccountType.BRONZE))
+
 
 }
 
 enum class AccountType(val discountPercentage: Int, val subs: Int) {
     BRONZE(5, 50) {
-        override fun calculate(): Int {
+        override fun calculateAbstract(): Int {
             return discountPercentage * subs
         }
     },
     SILVER(10, 100) {
-        override fun calculate(): Int {
+        override fun calculateAbstract(): Int {
             return discountPercentage * subs
         }
     },
     GOLD(15, 200) {
-        override fun calculate(): Int {
+        override fun calculateAbstract(): Int {
             return discountPercentage * subs
         }
     },
     PLATINUM(20, 150) {
-        override fun calculate(): Int {
+        override fun calculateAbstract(): Int {
             return discountPercentage * subs
         }
     },
     RUBY(35, 29) {
-        override fun calculate(): Int {
+        override fun calculateAbstract(): Int {
             return discountPercentage * subs
         }
     };
 
-    abstract fun calculate(): Int
+    // Local Fun
+    fun calculateLocal(accountType: AccountType): Int {
+        return accountType.discountPercentage * accountType.subs
+    }
+
+    abstract fun calculateAbstract(): Int
+
+    companion object {
+
+        fun calculate(accountType: AccountType): Int {
+            return accountType.calculateLocal(accountType)
+        }
+
+    }
+
 }
