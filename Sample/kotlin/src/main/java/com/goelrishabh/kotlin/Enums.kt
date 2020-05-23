e235b82a22d3dab722bfe627a9bd6f3b55eb82a2
Droid Developer <appdev@aerogram.in>
2020-05-23 23:03:02 +0530

    Enums with Abstract func

--- Sample/kotlin/src/main/java/com/goelrishabh/kotlin/Enums.kt
+++ Sample/kotlin/src/main/java/com/goelrishabh/kotlin/Enums.kt
@@ -11,12 +11,36 @@ fun main() {
     println(AccountType.PLATINUM.discountPercentage)
     println(AccountType.PLATINUM.subs)
 
+    println(AccountType.BRONZE.calculate())
+
 }
 
 enum class AccountType(val discountPercentage: Int, val subs: Int) {
-    BRONZE(5, 50),
-    SILVER(10, 100),
-    GOLD(15, 200),
-    PLATINUM(20, 150),
-    RUBY(35, 29)
+    BRONZE(5, 50) {
+        override fun calculate(): Int {
+            return discountPercentage * subs
+        }
+    },
+    SILVER(10, 100) {
+        override fun calculate(): Int {
+            return discountPercentage * subs
+        }
+    },
+    GOLD(15, 200) {
+        override fun calculate(): Int {
+            return discountPercentage * subs
+        }
+    },
+    PLATINUM(20, 150) {
+        override fun calculate(): Int {
+            return discountPercentage * subs
+        }
+    },
+    RUBY(35, 29) {
+        override fun calculate(): Int {
+            return discountPercentage * subs
+        }
+    };
+
+    abstract fun calculate(): Int
 }
