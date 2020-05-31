8011b3ec53a416a95f88755659a5b82acea37bd8
Droid Developer <appdev@aerogram.in>
2020-05-31 15:53:50 +0530

    Data Classes param components

--- Sample/kotlin/src/main/java/com/goelrishabh/kotlin/TestClass.kt
+++ Sample/kotlin/src/main/java/com/goelrishabh/kotlin/TestClass.kt
@@ -21,6 +21,8 @@ fun main() {
 
     val dataClass = DataClass("Rishabh Goel", 22)
     println(dataClass) // Prints from .toString()
+    println(dataClass.component1())
+    println(dataClass.component2())
     val normalClass = DataClass.NormalClass("Rishabh Goel", 22)
     print(normalClass) // Prints the object address
 
