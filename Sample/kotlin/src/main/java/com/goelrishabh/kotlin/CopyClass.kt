e47c8b1d7e7a7d7e1eec37e5ce34a1460037eb56
Droid Developer <appdev@aerogram.in>
2020-05-31 23:05:47 +0530

    Copy classes

---
+++ Sample/kotlin/src/main/java/com/goelrishabh/kotlin/CopyClass.kt
@@ -0,0 +1,10 @@
+package com.goelrishabh.kotlin
+
+/**
+ * @project Sample
+ * @author rishabh-goel on 31-05-2020
+ */
+data class CopyClass(val name: String, val list: List<String>) {
+
+
+}
--- Sample/kotlin/src/main/java/com/goelrishabh/kotlin/TestClass.kt
+++ Sample/kotlin/src/main/java/com/goelrishabh/kotlin/TestClass.kt
@@ -33,4 +33,37 @@ fun main() {
     val normalClass = DataClass.NormalClass("Rishabh Goel", 22)
     print(normalClass) // Prints the object address
 
+    println()
+    println()
+    println()
+
+    var user1 = DataClass("Rishabh Goel", 22)
+    var user2 = DataClass("Shubham Modi", 23)
+
+    println(user1)
+    println(user2)
+
+    user2 = user1.copy() // Useful when there are large object to reuse, "WORKS ON DATA CLASSES ONLY"
+
+    println(user2)
+
+    println()
+    println()
+    println()
+
+    // TODO Be aware of List as there is SHALLOW COPY not DEEP COPY
+
+    val list1 = listOf("A", "B", "C", "D")
+    var copyClass1 = CopyClass("Rishabh Goel", list1)
+
+    val list2 = listOf("E", "F", "G", "H")
+    var copyClass2 = CopyClass("Shubham Modi", list2)
+
+    println(copyClass1)
+    println(copyClass2)
+
+    copyClass2 = copyClass1.copy("Hello")
+
+    println(copyClass2)
+
 }
